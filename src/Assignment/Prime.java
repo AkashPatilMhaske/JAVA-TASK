b9007404bcf0accdb52e56c1be4cc876f7da794d
Akashmhaske <akashmhaske06900@gmail.com>
2022-12-06 22:31:06 +0530

    This is my first git commit and practice program

---
+++ src/Assignment/Prime.java
@@ -0,0 +1,21 @@
+package Assignment;
+
+public class Prime {
+    public static void main(String[] args) {
+
+        System.out.println(Prime.isPrime(14));
+    }
+private static boolean isPrime(int number){
+        boolean isPrime=true;
+        for (int i=2;i<number/2;i++){
+            if(number%i==0){
+                isPrime=false;
+                break;
+            }
+        }
+        return isPrime;
+}
+
+
+
+}
