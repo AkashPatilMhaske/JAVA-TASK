84054f29a14a7e6d6f2e4f6152fb85cdaf88e493
Akashmhaske <akashmhaske06900@gmail.com>
2022-12-06 22:35:18 +0530

    This is my first git commit and practice program

---
+++ src/DataType/Productofnumber.java
@@ -0,0 +1,33 @@
+package DataType;
+
+public class Productofnumber {
+    public static void main(String[] args) {
+
+      int num1=6;
+      double num2=8.2;
+      double product =num1*num2;
+        System.out.println("Product of two number is : "+product);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
